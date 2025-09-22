15caf6d884c51253f4a5cff621a0ebb07c63dc81
kevinccbsg <kevinccbsg@gmail.com>
2025-09-22 20:22:55 +0200

    feat: add syntax basic example

---
+++ examples/hello_world.js
@@ -0,0 +1,35 @@
+// console.log('Hello world!!');
+// comentario
+// varias lineas
+
+/*
+
+Comentario
+varias lineas
+
+*/
+
+/*
+var
+let
+const
+*/
+
+let num1 = 5;
+const num2 = 6;
+
+num1 = 10;
+
+/*
+PascalCase
+cammelCase
+snake_case
+kebab-case
+UPPERCASE
+*/
+const tvPrice = 12;
+
+const result = num1 + num2;
+
+console.log(num1, num2);
+console.log(result);
