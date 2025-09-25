d24fdf02b7a6c83fe530729c1863bf3fd06d91cf
kevinccbsg <kevinccbsg@gmail.com>
2025-09-25 21:02:20 +0200

    feat: add basic loop example file

---
+++ examples/07_bucles.js
@@ -0,0 +1,24 @@
+console.log('07');
+
+const fruits = [
+  'apple', // 0
+  'banana', // 1
+  'kiwi', // 2
+  'watermelon', // 3
+  'orange', // 4
+  'lemon', // 5
+];
+
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]);
+console.log(fruits[4]);
+console.log(fruits[5]);
+
+/*
+for, while, for of, for in
+*/
+for (/* definicion contador*/; /* condicion para iterar */; /* incremento */) {
+  console.log('Dentro del for');
+}
